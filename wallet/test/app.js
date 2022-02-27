67d89da6b5b65b6fdcfd07c38c25f1707035a8f6
mindol8 <37296580+mindol8@users.noreply.github.com>
2022-02-27 13:14:32 +0900

    demon

--- wallet/test/app.js
+++ wallet/test/app.js
@@ -1,9 +1,20 @@
 import pk from "../mnemonic/wallet.js";
 import mnemonic from "../mnemonic/mnemonic.js";
-console.log(mnemonic);
+import Web3 from "web3";
+/**
+ console.log(mnemonic);
 
 console.log(
-    pk(mnemonic, 'pw', 0, (pk) => {
-        console.log(pk);
-    })
+    
 )
+
+ */
+const bscTestnet = `https://speedy-nodes-nyc.moralis.io/11ed355a9e5af834c4ec415e/bsc/testnet`;
+const web3 = new Web3(new Web3.providers.HttpProvider(bscTestnet));
+
+pk(mnemonic, 'pw', 0, async (pk) => {
+    const account = web3.eth.accounts.privateKeyToAccount(pk);
+    console.log(account);
+    console.log(await web3.eth.getBalance(account.address));
+})
+
